1676cc05cdfffe1fbb4109cca870d9b417533da0
spidy-naik <166608100+spidy-naik@users.noreply.github.com>
2024-07-07 12:46:25 +0530

    config

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 import os
 
 #Bot token @Botfather
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7243457121:AAGrYohBljhlDZVfsKj8hJXTlby7-nrwojk")
 
 #Your API ID from my.telegram.org
-API_ID = int(os.environ.get("API_ID", ""))
+API_ID = int(os.environ.get("API_ID", "25412293"))
 
 #Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "d58456be9931f3f6b8154a626fc1b3c6")
 
 #Database 
-DB_URI = os.environ.get("DB_URI", "")
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://ruhani:iwC9EVMuenUdA4qY@ruhani.u8a2fhl.mongodb.net/?retryWrites=true&w=majority")
